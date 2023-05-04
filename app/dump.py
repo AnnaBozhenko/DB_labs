eed3010a118aec2919175f61c81cfe30124da34f
Anna Bozhenko <ms.anna.bozhenko.03@gmail.com>
2023-05-04 11:00:17 +0300

    Changed version of lab1, columns standartisation, convertion types of data functionality, query on insert to examinations.

--- app/dump.py
+++ app/dump.py
@@ -3,18 +3,6 @@ import re
 import csv
 import time
 
-# --------- QUERIES ----------
-query_create_insert_exams_info = """CREATE TABLE IF NOT EXISTS Insert_exams_info (
-    id serial,
-    year smallint,
-    total_rows integer,
-    wall_time numeric(10, 5),
-    process_time numeric(10, 5)
-    );"""
-
-query_populate_insert_exams_info = """INSERT INTO Insert_exams_info (year, total_rows, wall_time, process_time) 
-VALUES (2016, 0, 0, 0), (2017, 0, 0, 0), (2018, 0, 0, 0), (2019, 0, 0, 0), (2020, 0, 0, 0), (2021, 0, 0, 0)"""
-
 # Порівняти найгірший бал з Української мови та літератури у кожному регіоні у 2020 та
 # 2021 роках серед тих кому було зараховано тест
 comparison_query = """
@@ -38,15 +26,71 @@ join
 	group by regname) as statistics2021
 using(regname);"""
 
-# --------- VALUES ----------
+query_create_insert_log = """create table InsertLog (
+    year smallint primary key,
+    inserted_rows_n integer,
+    insertion_time numeric(10, 5)
+);"""
 
-db_user = "Student"
-db_pass = "qwerty"
-db_name = "ZNO"
-db_host = "db"
+# --------- VALUES ----------
+mapper_pattern_column = {"(st|out)id":         "OutId",                 
+                         "birth":              "Birth", 
+                         "sex.+":              "SexTypeName", 
+                         "regtypename":        "RegTypeName", 
+                         "tertypename":        "TerritoryTypeName",
+                         "classprofile":       "ClassProfileName", 
+                         "class(lang|reg)name":"ClassLangName",   
+                         "eotypename":         "InstitutionType",  
+                         "eoparent$":          "Parent",           
+                         "(eo|.+pt)name":      "InstitutionName",
+                         "(eo|.*pt)areaname":  "InstitutionAreaName", 
+                         "^areaname":          "StudentAreaName",
+                         "(eo|.*pt)regname":   "InstitutionRegionName",
+                         "^regname":           "StudentRegionName",
+                         "(eo|.*pt)tername":   "InstitutionTerritoryName",
+                         "^tername":           "StudentTerritoryName",  
+                         ".+test$":             "TestName",       
+                         ".+lang$":            "TestLanguage",   
+                         ".+ball12":           "Ball12",         
+                         ".+dpalevel":         "DPAlevel",       
+                         ".*adaptscale":       "AdaptScale",     
+                         ".+ball$":            "Ball",           
+                         ".+ball100":          "Ball100",        
+                         ".+teststatus":       "TestStatus",       
+                         ".*subtest":          "SubTest",
+                         "year":               "TestYear"}        
+
+mapper_pattern_test =  {"uml": "uml", 
+                        "ukr": "ukr", 
+                        "hist": "hist", 
+                        "math[^s]": "math",
+                        "mathst": "mathst", 
+                        "phys": "phys",
+                        "chem": "chem", 
+                        "bio": "bio", 
+                        "geo": "geo", 
+                        "eng": "eng", 
+                        "deu": "deu", 
+                        "rus": "rus", 
+                        "sp": "sp", 
+                        "spa": "sp", 
+                        "fr": "fr"}
+
+# db_user = "Student"
+# db_pass = "qwerty"
+# db_name = "ZNO"
+# db_host = "db"
+db_user="postgres"
+db_pass="turtle"
+db_name="learn_examples"
+db_host="localhost"
 
 loading_time = "./program_output/loading_time.txt"
 query_to_db = "./program_output/queries.csv"
+ddl_commands = "./migrations/V1__create_examinations.sql"
+dml_commands = "./migrations/V2__populate_examinations.sql"
+create_schema = "create_schema.sql"
+
 rows_to_write_numb = 1000
 extract_data = [(2016, "./data/OpenData2016.csv", "cp1251"),
                 (2017, "./data/OpenData2017.csv", "utf-8-sig"),
@@ -56,66 +100,92 @@ extract_data = [(2016, "./data/OpenData2016.csv", "cp1251"),
                 (2021, "./data/Odata2021File.csv", "utf-8-sig")]
 
 # --------- FUNCTIONALITY ----------
+def get_type(name):
+    name = name.lower()
+    if "ball" in name:
+        return "numeric(4, 1)"
+    elif "subtest" in name:
+        return "boolean"
+    elif any([el in name for el in["adaptscale", "birth", "testyear"]]):
+        return "smallint"
+    else:
+        return "varchar"
 
-def construct_query_create_zno():
-    columns = set()
+
+def format_value(column_name):
+    column_name = column_name.lower()
+    if "ball" in column_name:
+        return lambda x: x.replace(",", ".") if x != "null" else None
+    elif "subtest" in column_name:
+        return lambda x: None if x == "null" else "true" if x.lower() =="так" else "false"
+    else:
+        return lambda x: x if x != "null" else None
+
+
+def set_type(name):
+    if "ball" in name:
+        return lambda x: float(x)
+    elif "subtest" in name:
+        return lambda x: True if x.lower()=="так" else False
+    elif name in "adaptscale" or name in "birth" or name in "testyear":
+        return lambda x: int(x)
+    else:
+        return lambda x: x
+
+
+def format_column(name):
+    """name - unformatted string, representing column name;
+    returns formatted to standart column name."""
+    name = name.lower()
+    for pattern, column_name in mapper_pattern_column.items():
+        if bool(re.match(pattern, name)):
+            test = [v for k, v in mapper_pattern_test.items() if bool(re.match(k, name))]
+            test = test[0] if test != [] else ''
+            return test + column_name
+    raise Exception(f"Couldn't resolve name: {name}")
+
+
+def get_query_create_examinations():
+    """returns string, a query to create table Examinations"""
+    columns_names = []
     for year, filename, encoding in extract_data:
-        with open(file=filename, encoding=encoding) as f:
-            column_names = ['year'] + f.readline().strip("\n").replace("\"", "").lower().split(';')
-            [columns.add(el) for el in column_names]
-
-    columns = list(columns)
-    # change order of columns to improve insertion time
-    reordered_indeces = [columns.index('ukrsubtest')]
-    [reordered_indeces.append(columns.index(el)) if "ball" in el else None for el in columns]
-    [reordered_indeces.append(columns.index(el)) if el in ['birth', 'year'] or 'adaptscale' in el else None for el in columns]
-    [reordered_indeces.append(i) if i not in reordered_indeces else None for i  in range(len(columns))]
-    columns = [columns[i] for i in reordered_indeces]
-
+        with open(filename, encoding=encoding) as f:
+            reader = csv.reader(f, delimiter=";")
+            columns_names += [format_column(el) for el in next(reader)]
+    columns_types = [el + " " + get_type(el) for el in set(columns_names + ["TestYear"])]
+    query = "CREATE TABLE Examinations (" + ', '.join(columns_types) + ");"
     
-    create_q = "CREATE TABLE IF NOT EXISTS EXAMINATIONS ("
-    for col_name in columns:
-        if bool(re.search('\Afr[^a]', col_name)):
-            continue
-        elif bool(re.search('\Asp[^a]', col_name)):
-            continue
-        # set type to each column
-        if 'ball' in col_name:
-            create_q += f"{col_name} NUMERIC(4, 1), "
-        elif col_name in ['birth', 'year'] or 'adaptscale' in col_name:
-            create_q += f"{col_name} SMALLINT, "
-        elif col_name == 'ukrsubtest':
-            create_q += f"{col_name} BOOLEAN, "
-        else:
-            create_q += f"{col_name} VARCHAR, "
-    
-    create_q = create_q[:-2] + ");"
-    return create_q
+    return query
 
 
 def transaction(func):
     def inner(*args, **kwargs):
         conn = None
-        while True:
-            try:
-                conn = psycopg2.connect(host=db_host, dbname=db_name, user=db_user, password=db_pass)
-                func(conn, *args, **kwargs)
-                conn.commit()
-            except psycopg2.OperationalError as ex:
-                print(f"Failed to connect to database, error code: {ex.pgcode} \n {ex}")
-                time.sleep(2)
-            except psycopg2.DataError as ex:
-                conn.rollback()
-                print(f"Data error occured, error code: {ex.pgcode} \n {ex}")
-                quit()
-            except Exception as ex:
-                conn.rollback()
-                print(f"Program error occured: \n {ex}")
-                quit()
-            finally:
-                if conn is not None:
-                    conn.close()
-                    break
+        conn = psycopg2.connect(host=db_host, dbname=db_name, user=db_user, password=db_pass)
+        func(conn, *args, **kwargs)
+        conn.commit()
+        conn.close()
+
+        # while True:
+        #     try:
+                # conn = psycopg2.connect(host=db_host, dbname=db_name, user=db_user, password=db_pass)
+                # func(conn, *args, **kwargs)
+                # conn.commit()
+            # except psycopg2.OperationalError as ex:
+            #     print(f"Failed to connect to database, error code: {ex.pgcode} \n {ex}")
+            #     time.sleep(2)
+            # except psycopg2.DataError as ex:
+            #     conn.rollback()
+            #     print(f"Data error occured, error code: {ex.pgcode} \n {ex}")
+            #     quit()
+            # except Exception as ex:
+            #     conn.rollback()
+            #     print(f"Program error occured: \n {ex}")
+            #     quit()
+            # finally:
+            #     if conn is not None:
+                    # conn.close()
+                    # break
     return inner
 
 
@@ -143,49 +213,38 @@ def write_query(conn):
 
 @transaction
 def prepare_tables(conn):
-    query_create_examinations = construct_query_create_zno()
+    query_create_exams = get_query_create_examinations()
+    
     with conn.cursor() as cur:
-        cur.execute(query_create_examinations)
-        cur.execute(query_create_insert_exams_info)
-        cur.execute("select count(*) from Insert_exams_info;")
-        cur.execute(query_populate_insert_exams_info)
-
-
-def set_types(column_names, values):
-    for i in range(len(values)):
-        if values[i] == 'null':
-            values[i] = None
-        elif 'ball' in column_names[i]:
-            values[i] = float(values[i].replace(",", "."))
-        elif column_names[i] == 'birth' or 'adaptscale' in column_names[i]:
-            values[i] = int(values[i])
-        elif column_names[i] == 'ukrsubtest':
-            values[i] = True if values[i].lower() == 'так' else False
-    return values
+        cur.execute(query_create_exams)
+        cur.execute(query_create_insert_log)
+        cur.execute(f"insert into InsertLog (year, inserted_rows_n, insertion_time) values {', '.join(['%s']*len(extract_data))}", [(info[0], 0, 0) for info in extract_data])
 
 
 @transaction
 def populate_examinations(conn):
-    for year, filename, encoding in extract_data:
-        with open(filename, encoding=encoding, newline="") as f:
-            column_names = f.readline().replace("\"", "").replace("\r\n", "").lower().split(";") + ['year']
-            
-            for i in range(len(column_names)):
-                if bool(re.search('\Afr[^a]', column_names[i])):
-                    column_names[i] = 'fra' + column_names[i][2:]
-                elif bool(re.search('\Asp[^a]', column_names[i])):
-                    column_names[i] = 'spa' + column_names[i][2:]
-            
-            read_rows_n = 0
-            start_wall, start_cpu = time.time(), time.process_time()
-            with conn.cursor() as cur:
-                cur.execute("SELECT total_rows, wall_time, process_time FROM Insert_exams_info WHERE year = %s", (year,))
-                read_rows_n, passed_wall_time, passed_cpu_time = cur.fetchone()
-                passed_wall_time = float(passed_wall_time)
-                passed_cpu_time = float(passed_cpu_time)
-                
-                reader = csv.reader(f, delimiter=";")
+    def insert_to_tables(y, columns, values, cur, n):
+        query = f"INSERT INTO Examinations ({', '.join(columns)}) VALUES {', '.join(['%s']*len(values))};"
+        cur.execute(query, values)
+        cur.execute("UPDATE InsertLog SET inserted_rows_n = %s WHERE year = %s;", (n, y))
+        conn.commit()
 
+    for year, filename, encoding in extract_data:
+        print(f"year: {year}")
+        with open(filename, encoding=encoding) as f:
+            reader = csv.reader(f, delimiter=";")
+            column_names = next(reader) + ['year']
+            [print(before, after) for before, after in zip(column_names, [format_column(el) for el in column_names] )]
+            column_names = [format_column(el) for el in column_names] 
+            formatters = [format_value(c) for c in column_names]
+            
+            start_wall = time.time()
+            
+            with conn.cursor() as cur:
+                cur.execute(f"SELECT inserted_rows_n, insertion_time FROM InsertLog where year = {year};")
+                read_rows_n, passed_wall_time = cur.fetchone()
+                passed_wall_time = float(passed_wall_time)
+                
                 for _ in range(read_rows_n):
                     next(reader)
                 
@@ -193,32 +252,23 @@ def populate_examinations(conn):
                 values_array = []
                 for row in reader:
                     if counter == rows_to_write_numb:
-                        try:
-                            cur.execute(f"INSERT INTO Examinations ({','.join(column_names)}) VALUES {','.join(['('+','.join(['%s']*len(column_names))+')']*counter)};", values_array)
-                            values_array = []
-                            read_rows_n += counter
-                            cur.execute("UPDATE Insert_exams_info SET total_rows = %s WHERE year = %s;", (read_rows_n, year))
-                            conn.commit()
-                        except psycopg2.DataError as er:
-                            print(f"Error on insertion occured, error code: {er.pgcode} \n{er}")
-                            conn.rollback()
-                            break
-                        finally:
-                            counter = 0
+                        read_rows_n += counter
+                        insert_to_tables(year, column_names, values_array, cur, read_rows_n)
+                        counter = 0
 
                     row.append(year) 
-                    [values_array.append(el) for el in (set_types(column_names, row))]
+                    values_array.append(tuple([func(el) for func, el in zip(formatters, row)]))
                     counter += 1
                 
                 read_rows_n += counter
                 if counter > 0:
-                    cur.execute(f"INSERT INTO Examinations ({','.join(column_names)}) VALUES {','.join(['('+','.join(['%s']*len(column_names))+')']*counter)};", values_array)
+                    insert_to_tables(year, column_names, values_array, cur, read_rows_n)
+                    counter = 0
 
-                end_wall, end_cpu = time.time(), time.process_time()
+                end_wall = time.time()
                 passed_wall_time += end_wall - start_wall
-                passed_cpu_time += end_cpu - start_cpu
-                print(f"Insertion from the year {year}, process time: {passed_wall_time} seconds, cpu time: {passed_cpu_time} seconds")
-                cur.execute("UPDATE Insert_exams_info SET total_rows = %s, wall_time = %s, process_time = %s where year = %s;", (read_rows_n, passed_wall_time, passed_cpu_time, year))
+                print(f"Insertion from the year {year}, process time: {passed_wall_time} seconds")
+                cur.execute("UPDATE InsertLog SET insertion_time = %s where year = %s;", (passed_wall_time,))
                 conn.commit()
 
 
@@ -226,5 +276,5 @@ if __name__ == "__main__":
     prepare_tables()
     populate_examinations()
     print("Table examinations populated")
-    write_time()
-    write_query()
+    # write_time()
+    # write_query()
