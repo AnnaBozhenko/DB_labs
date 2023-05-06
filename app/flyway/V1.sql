76c6c27f7844937b0a83f73338e21656cce53891
dasha-ryzhkova <ryzhkovadasha2002@gmail.com>
2023-05-06 22:02:26 +0300

    added V1.sql (created tables)

---
+++ app/flyway/V1.sql
@@ -0,0 +1,57 @@
+DROP TABLE IF EXISTS Student;
+DROP TABLE IF EXISTS UkrTest;
+DROP TABLE IF EXISTS Test;
+DROP TABLE IF EXISTS Institution;
+DROP TABLE IF EXISTS LocationInfo;
+
+
+CREATE TABLE Student(
+    OutID               SERIAL NOT NULL PRIMARY KEY,
+    Birth               SMALLINT NOT NULL,
+    SexTypeName         VARCHAR NOT NULL,
+    InstitutionID       SERIAL NULL,
+    RegTypeName         VARCHAR NOT NULL,
+    ClassProfileName    VARCHAR NULL,
+    ClassLangName       VARCHAR NULL,
+    locationID          SERIAL NOT NULL
+);
+
+
+CREATE TABLE UkrTest(
+    testID          SERIAL NOT NULL PRIMARY KEY,
+    OutID           SERIAL NOT NULL PRIMARY KEY,
+    UkrAdaptScale   SMALLINT NULL,
+    UkrSubTest      BOOLEAN NULL
+);
+
+
+CREATE TABLE Test(
+    testID          SERIAL NOT NULL PRIMARY KEY,
+    OutID           SERIAL NOT NULL PRIMARY KEY,
+    subject         VARCHAR NULL,
+    lang            VARCHAR NULL,
+    DPALevel        VARCHAR NULL,
+    ball12          NUMERIC(4, 1) NULL,
+    ball            NUMERIC(4, 1) NULL,
+    ball100         NUMERIC(4, 1) NULL,
+    InstitutionID   SERIAL NULL,
+    testStatus      VARCHAR NULL,
+    testYear        SMALLINT NOT NULL
+);
+
+
+CREATE TABLE Institution(
+    InstitutionID       SERIAL NOT NULL PRIMARY KEY,
+    locationID          SERIAL NOT NULL,
+    parent              VARCHAR NOT NULL,
+    institutionType     VARCHAR NULL
+);
+
+
+CREATE TABLE LocationInfo(
+    locationID          SERIAL NOT NULL PRIMARY KEY,
+    regionName          VARCHAR NULL,
+    areaName            VARCHAR NULL,
+    territoryName       VARCHAR NULL,
+    territoryType       VARCHAR NULL
+);
