41afce8e4b6ea665c4b35ccaa036bac64c3b5f54
dasha-ryzhkova <ryzhkovadasha2002@gmail.com>
2023-05-08 16:41:16 +0300

    updated V2.sql (inserting for Student + update)

--- app/flyway/V2.sql
+++ app/flyway/V2.sql
@@ -35,26 +35,34 @@ UNION SELECT DISTINCT bioptname, NULL, NULL      FROM Examinations
 UNION SELECT DISTINCT physptname, NULL, NULL     FROM Examinations
 UNION SELECT DISTINCT mathstptname, NULL, NULL   FROM Examinations;
 
-
 UPDATE Institution
+SET locationID = LocationInfo.locationID
+WHERE Examinations.eoregname = LocationInfo.regionName AND
+      Examinations.eoareaname = LocationInfo.areaName  AND
+      Examinations.eotername = LocationInfo.territoryName;
+
+
+
+INSERT INTO Student(OutID, Birth, SexTypeName, RegTypeName, ClassProfileName, ClassLangName)
+SELECT DISTINCT outid, birth, sextypename, regtypename, classprofilename, classlangname;
+
+UPDATE Student
 SET locationID = LocationInfo.locationID
 WHERE Examinations.regname = LocationInfo.regionName AND
       Examinations.areaname = LocationInfo.areaName  AND
       Examinations.tername = LocationInfo.territoryName;
 
---
---
---
--- INSERT INTO Student(OutID, Birth, SexTypeName, InstitutionID, RegTypeName, ClassProfileName,
---                     ClassLangName, locationID)
---
---
---
---
+UPDATE Student
+SET InstitutionID = Institution.InstitutionID
+WHERE Examinations.eoname = Institution.institutionName AND
+      Examinations.eoparent = Institution.parent  AND
+      Examinations.eotypename = Institution.institutionType;
+
+
 -- INSERT INTO UkrTest(OutID, UkrAdaptScale, UkrSubTest)
 -- SELECT DISTINCT   stid,
 --
---
+
 --
 --
 --
