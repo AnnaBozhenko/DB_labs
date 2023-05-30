31d2162be03611b13260a37dfd85344132740868
dasha-ryzhkova <ryzhkovadasha2002@gmail.com>
2023-05-31 01:43:04 +0300

    fixed routes.py

--- webapp/app/routes.py
+++ webapp/app/routes.py
@@ -1,32 +1,32 @@
 from flask import render_template, url_for
-from app import app
+from webapp.app import app
 
 @app.route('/')
 @app.route('/', methods=['GET', 'POST'])
 def main_page():
     return render_template('index.html')
 
-@app.route('/location_info/', methods=['GET', 'POST'])
+@app.route('/location', methods=['GET', 'POST'])
 def location_info():
     return render_template('location.html')
 
 
-@app.route('/institution_info/', methods=['GET', 'POST'])
+@app.route('/institution', methods=['GET', 'POST'])
 def institution_info():
     return render_template('institution.html')
 
 
-@app.route('/student_info/', methods=['GET', 'POST'])
+@app.route('/student', methods=['GET', 'POST'])
 def student_info():
     return render_template('student.html')
 
 
-@app.route('/test_info/', methods=['GET', 'POST'])
+@app.route('/test', methods=['GET', 'POST'])
 def test_info():
     return render_template('test.html')
 
 
-@app.route('/queries_info/', methods=['GET', 'POST'])
+@app.route('/queries', methods=['GET', 'POST'])
 def queries():
     return render_template("queries.html", title="Queries")
 
