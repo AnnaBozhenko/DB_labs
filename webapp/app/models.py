57f2b4113a14dd78cf0d6a1ef628b40be468cd3c
Anna Bozhenko <ms.anna.bozhenko.03@gmail.com>
2023-05-31 00:02:32 +0300

    Database entities are mapped.

---
+++ webapp/app/models.py
@@ -0,0 +1,9 @@
+from sqlalchemy import MetaData, Table
+from app import engine
+
+metadata_obj = MetaData()
+
+LocationInfo = Table("locationinfo", metadata_obj, autoload_with=engine, schema="public")
+Institution = Table("institution", metadata_obj, autoload_with=engine, schema="public")
+Student = Table("student", metadata_obj, autoload_with=engine, schema="public")
+Test = Table("test", metadata_obj, autoload_with=engine, schema="public")
