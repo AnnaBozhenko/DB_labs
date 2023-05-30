b0143d4c5672c3734bd3d959e6c3bb402a18bfbc
Anna Bozhenko <ms.anna.bozhenko.03@gmail.com>
2023-05-30 13:09:29 +0300

    session function created.

--- webapp/app/__init__.py
+++ webapp/app/__init__.py
@@ -1,8 +1,22 @@
 from flask import Flask
 from config import Config
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
+
 
 app = Flask(__name__)
 app.config.from_object('config')
 app.config.from_object(Config)
+
+# prepare database connection
+engine = create_engine('postgresql+psycopg2://postgres:turtle@localhost/python_db')
+ 
+def db_session(func):
+    def inner(*args, **kwargs):
+        with Session(engine) as session:
+            func_call = func(*args, **kwargs)
+            session.commit()
+            return func_call
+    return inner
 
 from app import routes
