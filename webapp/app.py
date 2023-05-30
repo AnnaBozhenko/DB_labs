51a1262e9a629c17dbeb656956a8334ef56e0803
Anna Bozhenko <ms.anna.bozhenko.03@gmail.com>
2023-05-30 13:08:35 +0300

    primitive insert functions created.

--- webapp/app.py
+++ webapp/app.py
@@ -1,25 +1,86 @@
-from app import app
+from app import app, engine
+from webapp.app.models import LocationInfo, Institution, Student, Test
+from sqlalchemy import bindparam, insert, select
+
+def insert_into_locationInfo(values):
+    """values - list of dictionaries e.g [{'areaName': x, 'terName': y, 'regName': z}]"""
+    if values:
+        with engine.connect() as conn:
+            conn.execute(insert(LocationInfo).values(areaName = 'areaName', terName = 'terName', regName = 'regName'), values,)
+            conn.commit()
 
 
+def insert_into_institution(values):
+    """values - list of dictionaries e.g. [{'instName': , 'instType': , 'instParent': , 'areaName': , 'terName': , 'regName': }]"""
+    if values:
+    # check if location info about institution is present in LocationInfo table, if not - insert into location_info such info
+        with engine.connect() as conn:
+            locations_to_insert = []
+            for row in values:
+                subq = select(LocationInfo.c.id).where(LocationInfo.c.areaName == row['areaName'] and
+                                                       LocationInfo.c.terName == row['terName'] and
+                                                       LocationInfo.c.regName == row['regName'])
+                if not [row for row in conn.execute(subq)]:
+                    locations_to_insert.append({row['areaName'], row['terName'], row['regName']}) 
+            insert_into_locationInfo(locations_to_insert)
+    # perform insertion
+        with engine.connect() as conn:
+            select_locationId = select(LocationInfo.c.locationId).where(LocationInfo.c.areaName == bindparam('areaName') and 
+                                                                   LocationInfo.c.terName == bindparam('terName') and 
+                                                                   LocationInfo.c.regName == bindparam('regName'))
+            conn.execute(insert(Institution).values(locationId = select_locationId, 
+                                                    instName = 'instName',
+                                                    instType = 'instType',
+                                                    instParent = 'instParent'), values,)
+            conn.commit()
 
-# from sqlalchemy.ext.automap import automap_base
-# from sqlalchemy.orm import Session
-# from sqlalchemy import create_engine
 
-# Base = automap_base()
+def insert_into_student(values):
+    """values - list of dictionaries e.g. [{'outId': , 'birth': , 'sexTypeName': , 'regTypeName', 'areaName': , 'terName': , 'regName': , 'classProfileName', 'classLangName', 'eoName', 'eoAreaName', 'eoRegName', 'eoTerName'}]"""
+    if values:
+        locations_to_insert = []
+    # check if location info about student and sdudent's institution are present in LocationInfo table, if not - insert into location_info such info
+        with engine.connect() as conn:
+            for row in values:
+            # check student's location
+                subq = select(LocationInfo.c.id).where(LocationInfo.c.areaName == row['areaName'] and
+                                                       LocationInfo.c.terName == row['terName'] and
+                                                       LocationInfo.c.regName == row['regName'])
+                if not [row for row in conn.execute(subq)]:
+                    locations_to_insert.append({row['areaName'], row['terName'], row['regName']})
+            # check student's institution location
+                subq = select(LocationInfo.c.id).where(LocationInfo.c.areaName == row['eoAreaName'] and
+                                                       LocationInfo.c.terName == row['eoTerName'] and
+                                                       LocationInfo.c.regName == row['eoRegName'])
+                if not [row for row in conn.execute(subq)]:
+                    locations_to_insert.append({row['eoAreaName'], row['eoTerName'], row['eoRegName']})
+        insert_into_locationInfo(locations_to_insert)
+    # perform insertion
+        with engine.connect() as conn:
+            select_stud_locationId = select(LocationInfo.c.locationId).where(LocationInfo.c.areaName == bindparam('areaName') and 
+                                                                   LocationInfo.c.terName == bindparam('terName') and 
+                                                                   LocationInfo.c.regName == bindparam('regName'))
+            
+            # select_inst_locationId = select(LocationInfo.c.locationId).where(LocationInfo.c.areaName == bindparam('eoAreaName') and 
+            #                                                        LocationInfo.c.terName == bindparam('eoTerName') and 
+            #                                                        LocationInfo.c.regName == bindparam('eoRegName'))
+            
+            conn.execute(insert(Student).values(outId = 'outId',
+                                                birth = 'birth',
+                                                sexTypeName = 'sexTypeName',
+                                                locationId = select_stud_locationId, 
+                                                regTypeName = 'regTypeName',
+                                                classProfileName = 'classProfileName',
+                                                classLangName = 'classLangName',
+                                                institutionId = ''), values,)
+            conn.commit()
 
-# engine = create_engine('postgresql+psycopg2://user:password@host/database_name')
 
-# # reflect the tables
-# Base.prepare(autoload_with=engine)
-
-# LocationInfo = Base.classes.locationinfo
-# Institution = Base.classes.institution
-# Student = Base.classes.student
-# Test = Base.classes.test
+def insert_into_test(values):
+    pass
 
 
-# #session = Session(engine)
-# with Session(engine) as session:
-#     pass
-#     session.commit()
+@app.shell_context_processor
+def make_shell_context():
+    return {'LocationInfo': LocationInfo, 'Institution': Institution, 'Student': Student, 'Test': Test}
+
